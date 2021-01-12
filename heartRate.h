6dcddf794371a587fd7c3be60147398522d9fc50
Marlowe <g00325096@gmit.ie>
2021-01-12 01:14:59 +0000

    This is the header file for the heartRate to include with the cpp file

---
+++ heartRate.h
@@ -0,0 +1,8 @@
+#include <arduino.h>
+// #include "WProgram.h"
+
+
+bool checkForBeat(int32_t sample);
+int16_t averageDCEstimator(int32_t *p, uint16_t x);
+int16_t lowPassFIRFilter(int16_t din);
+int32_t mul16(int16_t x, int16_t y);
